757bffc1a00c37485ec3364227e0fce2b43ad8b3
hicx76 <brentmercer@brents-MacBook-Air.local>
2017-10-23 14:00:17 -0600

    Finished pixel art project.

--- designs.js
+++ designs.js
@@ -1,10 +1,30 @@
 // Select color input
 // Select size input
 
-// When size is submitted by the user, call makeGrid()
-
+// Take selected height and width, build table, then insert to pixel_canvas
 function makeGrid() {
-
-// Your code goes here!
-
+	let table;
+	let inputWidth = $('#input_width').val();
+	let inputHeight = $('#input_height').val();
+	let pixelCanvas = $('#pixel_canvas');
+	for (let i = 0; i < inputHeight; i++) {
+		table += "<tr>";
+		for (let j = 0; j < inputWidth; j++) {
+			table += "<td></td>";
+		}
+		table += "</tr>";
+	}
+	pixelCanvas.html(table);
 }
+
+// Listen for Submit button click and call makeGrid()
+$('#sizePicker').click(function(evt) {
+	evt.preventDefault();
+	makeGrid();
+})
+
+// Listen for click in cell and apply selected color
+$('#pixel_canvas').click(function(evt) {
+	let color = $('#colorPicker').val();
+	$(evt.target).css('background-color', color);
+})
